4e748a59f3c3e3ff2da0a21ccb4a48fa8aacef01
Eduardo Felipe <edufslima9@gmail.com>
2022-02-20 17:15:02 -0300

    Removing mocks and adding DB connection to application

--- RestWithASPNETUdemy/RestWithASPNETUdemy/Startup.cs
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Startup.cs
@@ -1,17 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using RestWithASPNETUdemy.Model.Context;
 using RestWithASPNETUdemy.Services;
 using RestWithASPNETUdemy.Services.Implementations;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace RestWithASPNETUdemy
 {
@@ -29,6 +24,9 @@ namespace RestWithASPNETUdemy
         {
 
             services.AddControllers();
+
+            var connection = Configuration["PostgreeConnection:PostgreeConnectionString"];
+            services.AddDbContext<PostgreSQLContext>(options => options.UseNpgsql(connection));
 
             // Dependency Injection
             services.AddScoped<IPersonService, PersonServiceImplementation>();
