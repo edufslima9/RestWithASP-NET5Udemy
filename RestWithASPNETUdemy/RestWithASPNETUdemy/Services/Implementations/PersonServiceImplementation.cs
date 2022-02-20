32041965b4eecf62aa21b2903bb1000958500a2e
Eduardo Felipe <edufslima9@gmail.com>
2022-02-20 15:04:24 -0300

    Create person service

---
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,69 @@
+﻿using RestWithASPNETUdemy.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Services.Implementations
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        private volatile int Count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+            
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> persons = new List<Person>();
+            for (int i = 0; i < 8; i++)
+            {
+                Person person = MockPerson(i);
+                persons.Add(person);
+            }
+            return persons;
+        }
+
+        public Person FindById(long id)
+        {
+            return new Person
+            {
+                Id = 1,
+                FirstName = "Eduardo",
+                LastName = "Lima",
+                Address = "Jardim São Paulo - Recife - Pernambuco",
+                Gender = "Male"
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = $"Person Name {i}",
+                LastName = "Person LastName",
+                Address = "Some Address",
+                Gender = i % 2 == 0 ? "Male" : "Female"
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref Count);
+        }
+    }
+}
